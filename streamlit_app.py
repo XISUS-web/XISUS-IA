067fc47c39316027478799c7c08cadb8bc7ea79c
XISUS-web <jesus.cabeza2008@gmail.com>
2026-09-01 20:56:26 +0200

    Change image URL in streamlit_app.py
    Updated image URL to a new link for display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,7 +101,7 @@ with st.sidebar:
 
 # --- AQUÍ VA TU IMAGEN ---
 # Puedes usar un enlace de internet directo (URL) o la ruta de un archivo local en tu GitHub
-URL_DE_TU_IMAGEN = "https://flaticon.com" 
+URL_DE_TU_IMAGEN = "https://i.postimg.cc/Gmx2FpG/IMG-2" 
 
 # Desplegamos la imagen centrada y con un tamaño estético de 200 píxeles
 st.image(URL_DE_TU_IMAGEN, width=200, use_container_width=False)
